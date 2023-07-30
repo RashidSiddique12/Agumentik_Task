43cb2e30dc58a804bea0add7d9ef888c092d828a
RashidSiddique12 <stylorashid786@gmail.com.com>
2023-07-30 22:26:47 +0530

    update

--- Backend/model/user.model.js
+++ Backend/model/user.model.js
@@ -1,4 +1,3 @@
-// backend/models/user.model.js
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -9,17 +9,14 @@ router.post("/register", async (req, res) => {
     const { email, password } = req.body;
     console.log(email);
 
-    // Check if the email is already registered
     const userExists = await User.findOne({ email });
     if (userExists) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // Hash the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // Create a new user
     const newUser = new User({ email, password: hashedPassword });
     await newUser.save();
 
@@ -35,19 +32,16 @@ router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Check if the user exists
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    // Validate the password
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
       return res.status(401).json({ message: "Invalid password" });
     }
 
-    // Generate a JWT token
     const token = jwt.sign({ _id: user._id }, config.jwtSecret);
 
     return res.status(200).json({ token });
